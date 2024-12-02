24689e85fd7fd84bba94ecbcec832ca6e8749d5f
Vedika-Patidar <vedika10patidar@gmail.com>
2024-12-02 17:28:22 +0530

    updated application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-// import logo from './devops.png';
+import logo from './devops.png';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello all welcome to React application</h1>
-        {/* <h2>hello all thanks for attending DevOps classes</h2> */}
+        <h2>hello all thanks for attending DevOps classes</h2>
         {/* <p>by VSCharan</p>   */}
       </header>
     </div>
